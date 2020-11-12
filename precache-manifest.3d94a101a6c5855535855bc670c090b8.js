e54f228cca77ce18c73de7a46126bebb53d94291
kwsantiago <kwsantiago@usf.edu>
2020-11-12 11:05:29 -0500

    Updates

---
+++ precache-manifest.3d94a101a6c5855535855bc670c090b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f6c6b9eabe36bfd9c664b97d6af4ac",
+    "url": "/CoinStriker/index.html"
+  },
+  {
+    "revision": "271c4208de0f452e0581",
+    "url": "/CoinStriker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f392594c879f1fe0b70",
+    "url": "/CoinStriker/static/js/2.fc7fa65c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CoinStriker/static/js/2.fc7fa65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271c4208de0f452e0581",
+    "url": "/CoinStriker/static/js/main.aa03d79c.chunk.js"
+  },
+  {
+    "revision": "173f943cbb21bfe2ba27",
+    "url": "/CoinStriker/static/js/runtime-main.612f9b1e.js"
+  }
+]);
