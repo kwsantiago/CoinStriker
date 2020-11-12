987fa981fbcc24513801284dce8794cd7262f66e
kwsantiago <kwsantiago@usf.edu>
2020-11-12 14:23:11 -0500

    Add Header image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,13 +9,7 @@ export class Header extends Component {
             <div className="container">
               <div className="row">
                 <div className="col-md-8 col-md-offset-2 intro-text">
-                  <h1>
-                    {this.props.data ? this.props.data.title : "Loading"}
-                    <span></span>
-                  </h1>
-                  <p>
-                    {this.props.data ? this.props.data.paragraph : "Loading"}
-                  </p>
+                  <div> <img src="img/header.png" className="img-responsive" alt=""/></div>
                   <a
                     href="#contact"
                     className="btn btn-custom btn-lg page-scroll"
