265395ae10e8f7628a49223b3553612c6d036e13
kwsantiago <kwsantiago@usf.edu>
2020-11-11 20:50:28 -0500

    Init with components

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Navigation from './components/navigation';
+import Header from './components/header';
+import About from './components/about';
+import Team from './components/Team';
+import Contact from './components/contact';
+import JsonData from './data/data.json';
+
+export class App extends Component {
+  state = {
+    landingPageData: {},
+  }
+  getlandingPageData() {
+    this.setState({landingPageData : JsonData})
+  }
+
+  componentDidMount() {
+    this.getlandingPageData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Header data={this.state.landingPageData.Header} />
+        <About data={this.state.landingPageData.About} />
+        <Team data={this.state.landingPageData.Team} />
+        <Contact data={this.state.landingPageData.Contact} />
+      </div>
+    )
+  }
+}
+
+export default App;
