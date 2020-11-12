64d09dcf7efe0fe6ee48888dfe57291a02c79421
kwsantiago <kwsantiago@usf.edu>
2020-11-12 11:47:57 -0500

    Investment Strategy NavBar Update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -29,28 +29,13 @@ export class Navigation extends Component {
           >
             <ul className="nav navbar-nav navbar-right">
               <li>
-                <a href="#features" className="page-scroll">
-                  Features
-                </a>
-              </li>
-              <li>
                 <a href="#about" className="page-scroll">
                   About
                 </a>
               </li>
               <li>
-                <a href="#services" className="page-scroll">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="page-scroll">
-                  Gallery
-                </a>
-              </li>
-              <li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
+                <a href="#investmentStrategy" className="page-scroll">
+                  Investment Strategy
                 </a>
               </li>
               <li>
