1741514e467442183f356930f4fee6b5decf7d90
kwsantiago <kwsantiago@usf.edu>
2020-11-12 23:13:35 -0500

    Updates

---
+++ precache-manifest.de59e420c51326c0a4c1b642edf9332e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00233f119f56bcbc70e75110d3ffedc8",
+    "url": "/CoinStriker/index.html"
+  },
+  {
+    "revision": "28ffaf6489a5d4807bf6",
+    "url": "/CoinStriker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc13e11103dcef1563fc",
+    "url": "/CoinStriker/static/js/2.eb54a881.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CoinStriker/static/js/2.eb54a881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ffaf6489a5d4807bf6",
+    "url": "/CoinStriker/static/js/main.f3e0ff85.chunk.js"
+  },
+  {
+    "revision": "173f943cbb21bfe2ba27",
+    "url": "/CoinStriker/static/js/runtime-main.612f9b1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b005556442d72638a3b0e764bd92342.js"
+  "/CoinStriker/precache-manifest.de59e420c51326c0a4c1b642edf9332e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CoinStriker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
