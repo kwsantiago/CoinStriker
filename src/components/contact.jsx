f3eb17dc80399890190a632c203767c38aef2c6b
kwsantiago <kwsantiago@usf.edu>
2020-11-16 11:08:16 -0500

    Updated Contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -87,20 +87,8 @@ export class Contact extends Component {
                 <div className="social">
                   <ul>
                     <li>
-                      <a
-                        href={this.props.data ? this.props.data.facebook : "/"}
-                      >
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
                       <a href={this.props.data ? this.props.data.twitter : "/"}>
                         <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href={this.props.data ? this.props.data.youtube : "/"}>
-                        <i className="fa fa-youtube"></i>
                       </a>
                     </li>
                   </ul>
